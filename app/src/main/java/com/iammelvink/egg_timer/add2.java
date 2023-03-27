b1e32db0a0b0bb5af626dc1447b865a4640ede36
unknown <afrin1907037@stud.kuet.ac.bd>
2023-03-28 02:34:14 +0600

    add2 java file added

---
+++ app/src/main/java/com/iammelvink/egg_timer/add2.java
@@ -0,0 +1,27 @@
+package com.iammelvink.egg_timer;
+
+public class add2 {
+    private int a;
+    private String str;
+
+    public add2(int a, String str) {
+        this.a = a;
+        this.str = str;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
